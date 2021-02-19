b758a3de069df0f62818b4e9e971a051cc6a8dbf
okikiola11 <okikiola.apelehin@gmail.com>
2021-02-19 14:49:08 +0000

    Update the index.html UI

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,12 @@
-const projectName = document.querySelector('#projectName');
+const projectInput = document.querySelector('#projectName');
 const addProjectBtn = document.querySelector('#modale_addProject_btn');
+const projectList = document.querySelector('#projects_list');
 
-export { projectName, addProjectBtn };
+const li =  document.createElement('li');
+li.id = 'listID';
+li.className = 'text';
+const paragraph =  document.createElement('p');
+
+export { projectInput, addProjectBtn,
+  projectList, li,
+paragraph };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import './styles/index.css';
-import { projectName, addProjectBtn } from './dom';
+
+import { projectInput, addProjectBtn,
+  projectList, projectLink, li, paragraph } from './dom';
+
+// addProjectBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+
+//   let value = projectInput.value;
+//   console.log(value);
+
+//   projectLink.appendChild(li);
+//   projectList.appendChild(projectLink);
+
+// });
+
+const addTodo = (todo, id) => {
+  // li.appendChild(paragraph);
+  
+  // li.value = '0';
+  const item = document.createElement('a');
+  item.appendChild(paragraph);
+  li.getElementsByClassName('text').id;
+  
+  li.appendChild(item);
+  projectList.appendChild(li);
+  paragraph.innerText = todo;
+  
+  console.log(projectList)
+
+  // li.push(todo);
+
+  const position = 'beforeend';
+
+  li.insertAdjacentHTML(position, item);
+   
+};
 
 addProjectBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  console.log(projectName.value);
+  const toDo = projectInput.value;
+  if (toDo) {
+    addTodo(toDo, id);
+  }
+  projectInput.value = '';
 });
+
