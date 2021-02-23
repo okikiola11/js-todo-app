eb9fca6b21de319b2259a76196ca1ac3b7b52f16
okikiola11 <okikiola.apelehin@gmail.com>
2021-02-23 12:07:04 +0000

    Add delete list button Add funtionality to the delete a single project list button Add the functionality to select a single list

--- src/index.js
+++ src/index.js
@@ -6,9 +6,25 @@ import './styles/index.css';
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'; //prevents you from overwriting the lists already inside the localstorage
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listsContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null;
+  saveAndRender();
+})
 
 newListForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -31,7 +47,8 @@ function saveAndRender() {
 }
 
 function save() {
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
@@ -41,6 +58,9 @@ function render() {
     listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
     listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list');
+    }
     listsContainer.appendChild(listElement);
   })
 }
